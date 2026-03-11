60f059392469f4245fe722eda0a036e3d223bceb
jon <jonathan.thermidor@outlook.com>
2026-03-11 23:58:50 +0100

    Test nearAllAttractions fixed.

--- TourGuide/src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
@@ -2,6 +2,7 @@ package com.openclassrooms.tourguide.service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -39,29 +40,49 @@ public class RewardsService {
 	}
 
 	public void calculateRewards(User user) {
+		System.out.println("Calculating rewards...");
 		List<VisitedLocation> userLocations = user.getVisitedLocations();
 		List<Attraction> attractions = gpsUtil.getAttractions();
 
-		//association attractions/rewards
-		Map<String, UserReward> userRewardMap = user.getUserRewards()
-				.stream()
-				.collect(Collectors.toMap(
-						userReward -> userReward.attraction.attractionName,
-						userReward -> userReward
-				));
+//        //association attractions/rewards > actual=52
+//        //tester avec set
+//        Map<String, UserReward> userRewardMap = user.getUserRewards()
+//                .stream()
+//                .collect(Collectors.toMap(
+//                        userReward -> userReward.attraction.attractionName,
+//                        userReward -> userReward
+//                ));
+
+		//rewarded attractions
+		//thread safe set(1)
+		Set<String> rewarded;
+		synchronized (user.getUserRewards()) {
+			rewarded = user.getUserRewards()
+					.stream()
+					.map(userReward -> userReward.attraction.attractionName)
+					.collect(Collectors.toSet());
+		}
 
 		for(VisitedLocation visitedLocation : userLocations) {
 			for(Attraction attraction : attractions) {
 				//rewarded ?
-//				if(user.getUserRewards().stream().noneMatch(r -> r.attraction.attractionName.equals(attraction.attractionName)))
-				if(!userRewardMap.containsKey(attraction.attractionName))
-			{
+	//				if(user.getUserRewards().stream().noneMatch(r -> r.attraction.attractionName.equals(attraction.attractionName)))
+				if(rewarded.contains(attraction.attractionName)) {
+					continue;}
 					if(nearAttraction(visitedLocation, attraction)) {
-						user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
-						userRewardMap.put(attraction.attractionName, new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
-					}
-				}
-			}
+						UserReward reward = new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user));
+
+                        //thread safe set(2)
+                        synchronized (user.getUserRewards()) {
+                            if (user.getUserRewards().stream()
+                                    .noneMatch(userReward -> userReward.attraction.attractionName.equals(attraction.attractionName))) {
+                                user.addUserReward(reward);
+                                rewarded.add(attraction.attractionName);
+
+                        }
+                    }
+                }
+            }
 		}
 	}
 
